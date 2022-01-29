6fb79e60dbda33474e32ca02e0e6b31efd190509
Lane Davis <lanedavis.csl@gmail.com>
2022-01-28 20:46:53 -0700

    Adding some toast show/hide state control.

--- Assets/scripts/InteractionToastData.cs
+++ Assets/scripts/InteractionToastData.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game Data/Interaction Toast")]
 public class InteractionToastData : ScriptableObject {
 
+    public float Time = 1f;
     public Color BackgroundColor;
+
+    public enum ToastScale {
+        none, small, large
+    }
+    public ToastScale Scale = ToastScale.small;
 
 }
