23b0380e446048d2fa3efe03413230f8eb0d9e1a
Lane Davis <lanedavis.csl@gmail.com>
2022-01-29 14:45:54 -0700

    Fixing some NPC animation issues and removing HLD from the camera.

--- Assets/scripts/NPCController.cs
+++ Assets/scripts/NPCController.cs
@@ -4,6 +4,17 @@ public class NPCController : MonoBehaviour {
 
     public InteractionToastData Toast;
     public GameObject FXRoot;
+
+    private bool firstUpdate;
+
+    private void FirstUpdate() {
+        firstUpdate = false;
+        UIObjectFX.DoEffect(UIObjectFX.EffectType.NPCPerkUpPulse, FXRoot, 0.1f);
+    }
+
+    private void Update() {
+        if (firstUpdate) FirstUpdate();
+    }
     
     public void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
