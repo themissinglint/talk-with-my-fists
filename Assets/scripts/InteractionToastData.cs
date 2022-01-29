fa6935d1ac624c569cad32cdc91b15a26f59e068
Lane Davis <lanedavis.csl@gmail.com>
2022-01-28 21:49:43 -0700

    Spending a bit too much time working on the toast display. Next: dialogue text!

--- Assets/scripts/InteractionToastData.cs
+++ Assets/scripts/InteractionToastData.cs
@@ -7,7 +7,11 @@ public class InteractionToastData : ScriptableObject {
 
     public float Time = 1f;
     public Color BackgroundColor;
+    public Sprite EntitySprite;
 
+    public Vector2 EntityScaleVector = Vector2.one;
+    public Vector2 EntityOffsetVector = Vector2.zero;
+    
     public enum ToastScale {
         none, small, large
     }
