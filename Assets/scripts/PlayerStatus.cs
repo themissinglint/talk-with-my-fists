dc7426030ee62e34802ed1482dde23cef3e51513
Lane Davis <lanedavis.csl@gmail.com>
2022-01-30 00:19:33 -0700

    Adding all the NPC dialogue and dialogue logic.

--- Assets/scripts/PlayerStatus.cs
+++ Assets/scripts/PlayerStatus.cs
@@ -1,10 +1,33 @@
 using System.Collections.Generic;
-using UnityEngine;
 
 public static class PlayerStatus {
 
+    // Acorn Quest
     public static int AcornCount;
-    public static bool HasCompletedGardenerQuest;
+    public static bool HasCompletedGardenerAcornQuest;
+    
+    // Mushroom Quest
+    public static int MushroomCount;
+    public static bool HasCompletedBlacksmithMushroomQuest;
+    
+    // Wood Quest
+    public static int WoodCount;
+    public static bool HasCompletedTraderWoodQuest;
+    
+    // Bee Slaying Quest
+    public static int BeeKillCount;
+    public static bool HasCompletedBlacksmithBeeQuest;
+    public static bool HasCompletedGardenerBeeQuest;
+    public static bool HasCompletedGardenerSecondBeeQuest;
+    
+    // Dinosaur Slaying Quest
+    public static bool HasKilledDinosaur;
+    public static bool HasCompletedDinosaurKillQuest;
+    
+    // Trader Ladder Quest
+    public static bool HasDestroyedTraderLadder;
+    public static bool HasCompletedTraderLadderQuest;
+    
 
     public static Dictionary<PlayerStat, float> StatValues = new Dictionary<PlayerStat, float> {
         {PlayerStat.Achievement, 0f},
