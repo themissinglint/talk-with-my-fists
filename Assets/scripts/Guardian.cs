9a71db6faec05620e5494d29110f464354c1d94b
Shanti Pothapragada <themissinglint@gmail.com>
2022-01-30 15:43:04 -0600

    add comments to Guardian.cs

--- Assets/scripts/Guardian.cs
+++ Assets/scripts/Guardian.cs
@@ -7,10 +7,10 @@ public class Guardian: MonoBehaviour
 {
 	public Transform home, charge, retreat;
 	public float speed;
+	public int hpToRampage = 10;
 	private int patrolPointIdx;
 	private Rigidbody2D rb;
 	private SpriteRenderer spriteRenderer;
-	private Vector3[] patrolPoints;
 	private Vector3 chargeTarget;
 	private Vector3 homeTarget;
 	private Vector3 retreatTarget;
@@ -28,6 +28,7 @@ public class Guardian: MonoBehaviour
 		rb = GetComponent<Rigidbody2D>();
 		spriteRenderer = GetComponent<SpriteRenderer>();
 
+		//these take the position at game start time:
 		homeTarget = home.position;
 		chargeTarget = charge.position;
 		retreatTarget = retreat.position;
@@ -36,34 +37,44 @@ public class Guardian: MonoBehaviour
 	// Update is called once per frame
 	void Update() {
 
+		// default goal is home:
 		Vector3 target = homeTarget;
 
+		//TODO: make this not happen until player is watching.
 		if (!charging && PlayerStatus.StatValues[PlayerStat.Social] > .8f) {
 			retreating = true;
 		}
 
+		// charge if HP dropped:
 		if (dmgStats.hp < lastHP) {
 			charging = true;
 			lastHP = dmgStats.hp;
 		}
 
+		// note charging/retreating may have been set in previous frames.
 		if (charging) {
 			target = chargeTarget;
 		} else if(retreating) {
 			target = retreatTarget;
 		}
 
-		// this is all in 1D (just X):
+		//Note this is all in 1D (just X):
+
+		//Move towards target:
 		rb.AddForce(new Vector3(Mathf.Sign(target.x - transform.position.x), 0f, 0f) * speed);
 
+		// stop charing if I got to the charge target:
 		if(charging && Mathf.Abs(transform.position.x - chargeTarget.x) < .4f) {
 			charging = false;
 		}
-		if (lastHP < 8 && Mathf.Abs(transform.position.x - homeTarget.x) < .4f) {
+
+		//if HP is low enough, keep charing over and over:
+		if (lastHP <= hpToRampage && Mathf.Abs(transform.position.x - homeTarget.x) < .4f) {
 			charging = true;
 		}
+
 		// flip sprite towards target:
-		spriteRenderer.flipX = transform.position.x < patrolPoints[patrolPointIdx].x;
+		spriteRenderer.flipX = transform.position.x < target.x;
 	}
 
 }
